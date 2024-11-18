fc8c6d43650b024ead363fe0f02e14f5580950a9
Yuri²² <00001088564045sp@al.educacao.sp.gov.br>
2024-11-17 22:36:04 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Old School',
+    'Entre Ronnie Coleman, Kevin Levrone e Arnold Schwarzenegger quem era mais forte?',
+    'O rei Coleman ganha esse título, conseguindo levantar impressionantemente mais de 1 TONELADA no leg press 45° e fazer agachamento livre com +360 KG!'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Ursos de força',
+    'Já pensou em fazer supino reto com 300KG?',
+    'O Andrey Smaev, fez 3 reps'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Aesthetic',
+    'Qual o nome do jovem mais estetico atual?',
+    'David Laid! Ele começou sendo um magro qualquer para um físico estético e bem definido, com suas postagens nas redes socias ele motiva vários a ter o shape'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Superação',
+    'Qual o homem que teve a maior superação de vida?',
+    'DAVID GOGGINS !!! É um ex-Navy SEAL, ultramaratonista e autor famoso por sua filosofia de superação mental e resistência extrema. Ele enfrentou uma infância difícil, mas se tornou um exemplo de resiliência ao alcançar conquistas físicas e psicológicas extraordinárias. Seu livro Cant Hurt Me e suas palestras motivacionais inspiram muitos a desafiar seus próprios limites.'
 )
