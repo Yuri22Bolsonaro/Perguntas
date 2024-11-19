44c7e64cdc96a07963f27f896c4c7ce8b9079a01
Yuri²² <00001088564045sp@al.educacao.sp.gov.br>
2024-11-19 13:32:55 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,17 @@
 criaCartao(
-    'Old School',
+    '<i>Old School</i>',
     'Entre Ronnie Coleman, Kevin Levrone e Arnold Schwarzenegger quem era mais forte?',
     'O rei Coleman ganha esse título, conseguindo levantar impressionantemente mais de 1 TONELADA no leg press 45° e fazer agachamento livre com +360 KG!'
 )
 
 criaCartao(
-    'Ursos de força',
+    '<i>Ursos de força</i>',
     'Já pensou em fazer supino reto com 300KG?',
     'O russo, Andrey Smaev, conhecido por sua força e tamanho de urso, conseguiu realizar 3 repetições com esse peso, uma marca impressionante no powerlifting. Esse feito coloca Smaev entre os atletas de elite no supino, sendo um exemplo de força brutal no esporte.'
 )
 
 criaCartao(
-    'Aesthetic',
+    '<i>Aesthetic Inspiration</i>',
     'Qual o nome do jovem mais estetico atual?',
     'David Laid! Ele começou sendo um magro qualquer para um físico estético e bem definido, com suas postagens nas redes socias ele motiva vários a ter o shape'
 )
@@ -19,5 +19,5 @@ criaCartao(
 criaCartao(
     'Superação',
     'Qual o homem que teve a maior superação de vida?',
-    'DAVID GOGGINS !!! É um ex-Navy SEAL, ultramaratonista e autor famoso por sua filosofia de superação mental e resistência extrema. Ele enfrentou uma infância difícil, mas se tornou um exemplo de resiliência ao alcançar conquistas físicas e psicológicas extraordinárias. Seu livro Cant Hurt Me e suas palestras motivacionais inspiram muitos a desafiar seus próprios limites. ~"They dont know me son" Stay hard!'
+    'DAVID GOGGINS !!! É um ex-Navy SEAL, o único integrante das Forças Armadas dos Estados Unidos a ter concluído o treinamento das três principais tropas de elite, ultramaratonista e autor famoso por sua filosofia de superação mental e resistência extrema. Ele enfrentou uma infância difícil, mas se tornou um exemplo de resiliência ao alcançar conquistas físicas e psicológicas extraordinárias. Seu livro <i>Cant Hurt Me</i> e suas palestras motivacionais inspiram muitos a desafiar seus próprios limites. ~"<i>They dont know me son</i>"'
 )
